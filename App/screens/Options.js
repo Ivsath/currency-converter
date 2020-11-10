c0e086f6e3546d34021438017d1d964fb5926237
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-10 17:05:13 -0600

    Add scrollview to the options list

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, ScrollView } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 
 import { RowItem, RowSeparator } from '../components/RowItem'
@@ -7,26 +7,28 @@ import colors from '../constants/colors'
 
 const Options = () => {
   return (
-    <SafeAreaView>
-      <RowItem
-        text="Themes"
-        onPress={() => alert('todo!')}
-        rightIcon={
-          <Entypo name="chevron-right" size="20" color={colors.blue} />
-        }
-      />
-      <RowSeparator />
-      <RowItem
-        text="React Native"
-        onPress={() => alert('todo!')}
-        rightIcon={<Entypo name="export" size="20" color={colors.blue} />}
-      />
-      <RowSeparator />
-      <RowItem
-        text="Flutter"
-        onPress={() => alert('todo!')}
-        rightIcon={<Entypo name="export" size="20" color={colors.blue} />}
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
+        <RowItem
+          text="Themes"
+          onPress={() => alert('todo!')}
+          rightIcon={
+            <Entypo name="chevron-right" size="20" color={colors.blue} />
+          }
+        />
+        <RowSeparator />
+        <RowItem
+          text="React Native"
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size="20" color={colors.blue} />}
+        />
+        <RowSeparator />
+        <RowItem
+          text="Flutter"
+          onPress={() => alert('todo!')}
+          rightIcon={<Entypo name="export" size="20" color={colors.blue} />}
+        />
+      </ScrollView>
     </SafeAreaView>
   )
 }
