a04755e0e41cc0c4f4cab06111a1273189ef78b7
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-10 23:48:21 -0600

    Add images in the home screen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, StyleSheet, StatusBar } from 'react-native'
+import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native'
 
 import colors from '../constants/colors'
+
+const screen = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.blue,
     flex: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoBackground: {
+    width: screen.width * 0.45,
+    height: screen.width * 0.45,
+  },
+  logo: {
+    position: 'absolute',
+    width: screen.width * 0.25,
+    height: screen.width * 0.25,
   },
 })
 
@@ -14,6 +30,18 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/images/background.png')}
+          style={styles.logoBackground}
+          resizeMode="contain"
+        />
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   )
 }
