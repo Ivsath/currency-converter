47122b5b675d59ae7cc05ee7dd0d0bc8649f3438
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-11 20:08:01 -0600

    Create button component

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -10,6 +10,7 @@ import {
 import { format } from 'date-fns'
 
 import { ConversionInput } from '../components/ConversionInput'
+import { Button } from '../components/Button'
 import colors from '../constants/colors'
 
 const screen = Dimensions.get('window')
@@ -90,6 +91,7 @@ const Home = () => {
           'MMMM dd, yyyy',
         )}`}
       </Text>
+      <Button text="Reverse Currencies" onPress={() => alert('todo!')} />
     </View>
   )
 }
