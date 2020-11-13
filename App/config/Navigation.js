8c1bb79386024ca068d963ea8936d8d296309855
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-13 14:19:43 -0600

    Display a screen as modal

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Entypo } from '@expo/vector-icons'
 
 import Home from '../screens/Home'
 import Options from '../screens/Options'
 import CurrencyList from '../screens/CurrencyList'
+import colors from '../constants/colors'
 
 const MainStack = createStackNavigator()
 const MainStackScreen = () => {
@@ -16,17 +19,41 @@ const MainStackScreen = () => {
         options={{ headerShown: false }}
       />
       <MainStack.Screen name="Options" component={Options} />
-      <MainStack.Screen
-        name="CurrencyList"
-        component={CurrencyList}
-        options={({ route }) => ({ title: route.params && route.params.title })}
-      />
     </MainStack.Navigator>
   )
 }
 
+const ModalStack = createStackNavigator()
+const ModalStackScreen = () => (
+  <ModalStack.Navigator mode="modal">
+    <ModalStack.Screen
+      name="Main"
+      component={MainStackScreen}
+      options={{ headerShown: false }}
+    />
+    <ModalStack.Screen
+      name="CurrencyList"
+      component={CurrencyList}
+      options={({ navigation, route }) => ({
+        title: route.params && route.params.title,
+        headerLeft: null,
+        headerRight: () => (
+          <TouchableOpacity onPress={() => navigation.pop()}>
+            <Entypo
+              name="cross"
+              size={30}
+              color={colors.blue}
+              style={{ paddingHorizontal: 10 }}
+            />
+          </TouchableOpacity>
+        ),
+      })}
+    />
+  </ModalStack.Navigator>
+)
+
 export default () => (
   <NavigationContainer>
-    <MainStackScreen />
+    <ModalStackScreen />
   </NavigationContainer>
 )
